bbffb0c4a1035ef096530902ad33757c1aaa3f9b
Rashed Ahmed <rashed.ahmed@email.com>
2023-03-29 20:35:29 +0600

    redux setup and dark-light mode

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import Head from 'next/head'
 import NavBar from './NavBar'
 import Footer from './Footer'
 
 const Layout = ({ children, title }) => {
+	const mode = useSelector(state => state.global.mode)
+	useEffect(() => {
+		const bodyClass = document.querySelector('body').classList
+		if (mode) {
+			bodyClass.add('light')
+			bodyClass.remove('dark')
+		} else {
+			bodyClass.add('light')
+			bodyClass.remove('dark')
+		}
+	}, [mode])
+
 	return (
 		<>
 			<Head>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import '../styles/bootstrap.scss'
 import '../styles/index.scss'
+import { configureStore } from '@reduxjs/toolkit'
+import globalReducer from '../redux'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+	reducer: {
+		global: globalReducer,
+	},
+})
 
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	)
 }
 
 export default MyApp
