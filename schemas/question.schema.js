a5e2157371a9dd5abc140ade7f4988791131c261
Rashed Ahmed <rashed.ahmed@email.com>
2023-04-07 22:45:04 +0600

    install zod | question schema

---
+++ schemas/question.schema.js
@@ -0,0 +1,20 @@
+import z from 'zod'
+
+export const createQuestionFormSchema = z.object({
+	image: z.string().url(),
+	title: z.string().min(1, { message: 'Title is required' }).max(100, 'Max length is 100'),
+	description: z.string().max(500, 'Max length is 500').optional(),
+	questions: z.array(questionSchema),
+})
+
+export const questionSchema = z.object({
+	title: z.string().min(1, { message: 'Title is required' }).max(100, 'Max length is 100'),
+	description: z.string().max(500, 'Max length is 500').optional(),
+	type: z.enum(['short', 'long', 'mcq', 'trfl']),
+	options: z.array(optionSchema),
+})
+
+export const optionSchema = z.object({
+	title: z.string().min(1, { message: 'Title is required' }).max(100, 'Max length is 100'),
+	name: z.string().min(1, { message: 'Name is required' }).max(5),
+})
