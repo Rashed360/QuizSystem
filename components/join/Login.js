531666c47404f4d156af4ed47569bc00b3e9e75a
Rashed Ahmed <rashed.ahmed@email.com>
2023-05-07 22:04:45 +0600

    login validation

--- components/join/Login.js
+++ components/join/Login.js
@@ -2,32 +2,61 @@ import Link from 'next/link'
 import Logo from 'assets/logoText.svg'
 import AnimatedLogo from 'components/AnimatedLogo'
 import Image from 'next/image'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { loginFormSchema } from 'schemas/user.schema'
 
 const Login = ({ toggler }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(loginFormSchema),
+	})
+
+	const onSubmit = async values => {
+		console.log('login successful', values)
+	}
+
 	return (
 		<div className='joinForm'>
-			<div class='mb-3'>
-				<Link href='/' class='navbar-brand'>
+			<div className='mb-3'>
+				<Link href='/' className='navbar-brand'>
 					<AnimatedLogo />
 					<Image src={Logo} alt='' />
 					Login
 				</Link>
 			</div>
-			<div class='form-text mb-3'>Provide correct email and password to login.</div>
-			<form>
-				<div class='mb-3'>
-					<label class='form-label'>Email address</label>
-					<input type='email' class='form-control' />
+			<div className='form-text mb-3'>Provide correct email and password to login.</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className='form-block mb-3'>
+					<label className='form-label'>Email address</label>
+					<input
+						type='email'
+						className='form-control'
+						placeholder='user@example.com'
+						{...register('email')}
+						aria-invalid={errors.email ? 'true' : 'false'}
+					/>
+					{errors.email && <span>{errors.email.message}</span>}
 				</div>
-				<div class='mb-3'>
-					<label class='form-label'>Password</label>
-					<input type='password' class='form-control' />
+				<div className='form-block mb-3'>
+					<label className='form-label'>Password</label>
+					<input
+						type='password'
+						className='form-control'
+						placeholder='••••••••••••••••'
+						{...register('password')}
+						aria-invalid={errors.password ? 'true' : 'false'}
+					/>
+					{errors.password && <span>{errors.password.message}</span>}
 				</div>
-				<div class='mb-3 form-check'>
-					<input type='checkbox' class='form-check-input' />
-					<label class='form-check-label'>Remember Me</label>
+				<div className='mb-3 form-check'>
+					<input type='checkbox' className='form-check-input' />
+					<label className='form-check-label'>Remember Me</label>
 				</div>
-				<button type='submit' class='btn btn-success me-3'>
+				<button type='submit' className='btn btn-success me-3'>
 					Login Now
 				</button>
 				<button className='btn' onClick={toggler}>
---
+++ schemas/user.schema.js
@@ -0,0 +1,31 @@
+import z from 'zod'
+
+export const loginFormSchema = z.object({
+	email: z.string().min(1, { message: 'Email is required' }).email({ message: 'Must be a valid email' }),
+	password: z
+		.string()
+		.min(1, { message: 'Password must contain special symbols' })
+		.min(8, { message: 'Password must be 8 characters long' })
+		.regex(/(?=.*[0-9])/, { message: 'Invalid Password.' })
+		.regex(/(?=.*[!@#$%^&*])/, { message: 'Invalid Password.' }),
+})
+
+export const signUpFormSchema = z
+	.object({
+		fullName: z.string().min(1, { message: 'First name is required' }),
+		email: z.string().min(1, { message: 'Email is required' }).email({ message: 'Must be a valid email' }),
+		password: z
+			.string()
+			.min(1, { message: 'Password is Required' })
+			.min(8, { message: 'Must be 8 characters long' })
+			.regex(/(?=.*[0-9])/, { message: 'Must contain at-least one number.' })
+			.regex(/(?=.*[!@#$%^&*])/, { message: 'Must contain special symbols.' }),
+		confirmPassword: z
+			.string()
+			.min(1, { message: 'Confirm Password is Required' })
+			.min(8, { message: 'Must be 8 characters long' }),
+	})
+	.refine(data => data.password === data.confirmPassword, {
+		path: ['confirmPassword'],
+		message: "Passwords don't match",
+	})
